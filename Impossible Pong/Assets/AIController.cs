ae927eea1b9b45e02ac52ee455aceac2480edb34
Andres Lauder <camodres@gmail.com>
2022-11-09 23:38:54 -0800

    enemy AI and paddle bigger
    I was able to make opposing paddle score decrease paddle, and the other would increase... as well as making a seperate player 2 paddle and instead giving it AI

---
+++ Impossible Pong/Assets/AIController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    public Transform ball;
+
+    [Range(0,1)]
+    public float skill;
+    private void FixedUpdate() {
+        Vector2 newPos = transform.position;
+        newPos.y = Mathf.Lerp(transform.position.y, ball.position.y, skill);
+        transform.position = newPos;
+    }
+}
--- Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Size-Paddle_Scripts/Size_Paddle_Script.cs
+++ Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Size-Paddle_Scripts/Size_Paddle_Script.cs
@@ -9,43 +9,56 @@ public class Size_Paddle_Script : MonoBehaviour
 
     void Start()
     {
-        player_1 = GameObject.Find("Player1").GetComponent<Transform>();
-        player_2 = GameObject.Find("Player2").GetComponent<Transform>();        
+        player_1 = GameObject.Find("Player 1").GetComponent<Transform>();
+        player_2 = GameObject.Find("Player 2").GetComponent<Transform>();        
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.name == "Right Border")
         {
-            StartCoroutine(ChangeSize_Paddle_Player());
-            Debug.Log("Player shrunk");
+            StartCoroutine(Increase_Paddle_Opponent());
+            Debug.Log("Opponent shrunk");
+            StartCoroutine(Decrease_Paddle_Player());
+
         }
 
         else if (collision.gameObject.name == "Left Border")
         {
-            StartCoroutine(ChangeSize_Paddle_Opponent());
-            Debug.Log("Opponent shrunk");
+            StartCoroutine(Increase_Paddle_Player());
+            Debug.Log("Player shrunk");
+            StartCoroutine(Decrease_Paddle_Opponent());
+
         }
     }
 
-    public IEnumerator ChangeSize_Paddle_Player(){
+    public IEnumerator Increase_Paddle_Player(){
         // OLS grabs the origional scale of the object
         Vector3 originalLocalScale = player_1.transform.localScale;
         // scale of object is added with a new vector 3
-        player_1.transform.localScale += new Vector3(-0.2F, -0.2f, -0.2f);
-        // wait 15 seconds
-        yield return new WaitForSeconds(15f);
-        // now set the origional scale 
-        player_1.transform.localScale = originalLocalScale;
+        player_1.transform.localScale += new Vector3(-0.2F, -0.2f, 0);
+        yield return new WaitForSeconds(0);
     }
 
-    public IEnumerator ChangeSize_Paddle_Opponent(){
+    public IEnumerator Increase_Paddle_Opponent(){
         // OLS grabs the origional scale of the object
         Vector3 originalLocalScale = player_2.transform.localScale;
         // scale of object is added with a new vector 3
-        player_2.transform.localScale += new Vector3(-0.2F, -0.2f, -0.2f);
-        // wait 15 seconds
-        yield return new WaitForSeconds(15f);
-        // now set the origional scale 
-        player_2.transform.localScale = originalLocalScale;
+        player_2.transform.localScale += new Vector3(-0.2F, -0.2f, 0);
+        yield return new WaitForSeconds(0);
+
+    }
+
+    public IEnumerator Decrease_Paddle_Player(){
+        // OLS grabs the origional scale of the object
+        Vector3 originalLocalScale = player_1.transform.localScale;
+        // scale of object is added with a new vector 3
+        player_1.transform.localScale += new Vector3(0.2F, 0.2f, 0);
+        yield return new WaitForSeconds(0);
+    }
+
+    public IEnumerator Decrease_Paddle_Opponent(){
+        player_2.transform.localScale += new Vector3(0.2F, 0.2f, 0);
+        yield return new WaitForSeconds(0);
+
     }
 }
