4e82bf8cf74098e8fc6925e7a7a16e648e2bd357
Andres Lauder <camodres@gmail.com>
2022-11-25 19:37:28 -0800

    I was able to fix The Multiple Gamemode
    I went into the scripting and was able to take ball_count (set to 0) and increase it by how many I wanted balls to be spawned into the game. This variable checked how many balls were being instantiated. I also put all the if statement functionality originally happening on the collision method and put it into one function. That way it can check if the limit is over or not, and depending on what is returned true, the function will execute. I'm not sure if I fixed the issue because I don't want it to still continue to spawn but we'll see.... more on the next dev day!

--- Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Multiple_Ball_Scripts/Ball_Spawn_Script.cs
+++ Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Multiple_Ball_Scripts/Ball_Spawn_Script.cs
@@ -8,29 +8,59 @@ public class Ball_Spawn_Script : MonoBehaviour
     public GameObject ball_prefab;
     public BallMovement ballMovement;
 
-    public int ball_count = 0;
+    private int ball_count = 0;
+
+    public int ball_limiter = 3;
+
+    public int ball_spawned = 0;
+
+    // make a variable that checks how many balls are in the game
+    //public GameObject[] ball_holder;
 
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+
+    // use this timer to limit the amount of balls based on the variable
+
+    private void Start()
+    {
+        // check how many balls are in the game --> shoot a message in console
+        //ball_holder = GameObject.FindGameObjectsWithTag("Duplicated_Ball");
+        ball_count++;
+        Debug.Log("There are " + ball_count + " in game");
+    }
 
 
-        if (collision.gameObject.name == "Right Border" || collision.gameObject.name == "Left Border")
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Right Border" && ball_spawned == 1 || collision.gameObject.name == "Left Border" && ball_spawned == 1)
         {
-            if (ball_count <= 2){
-                InvokeRepeating("SpawnMultipleBalls", 4, 4);
-            }
-            else{
-                CancelInvoke("SpawnMultipleBalls");
-            }
+        // if ball_count is greater than ball_limiter --> spawn
+            InvokeRepeating("SpawnMultipleBalls", 1.0f, 1.0f);
+            Debug.Log("Ball are being spawned");
+
 
             ballMovement.player1Start = false;
             // begin launch/reset function
             StartCoroutine(ballMovement.Launch());
         }
+        // ISSUE: after the entire thing runs through, it can still spawn more
     }
 
-    void SpawnMultipleBalls(){
-        Instantiate(ball_prefab);
-        ball_count++;
+    void SpawnMultipleBalls()
+    {
+
+        if (ball_count <= ball_limiter)
+        {
+            ball_spawned++;
+            Instantiate(ball_prefab);
+            ball_count++;
+            // displays to the console how many balls are in the game --> keeps getting bigger
+            Debug.Log("There are " + ball_count + " in game");
+        }
+        else
+        {
+            Debug.Log("balls are being limited");
+            CancelInvoke("SpawnMultipleBalls");
+        }
     }
 }
