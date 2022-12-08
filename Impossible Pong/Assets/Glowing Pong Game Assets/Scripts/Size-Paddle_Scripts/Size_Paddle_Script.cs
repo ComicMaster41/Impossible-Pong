aa867512018702b69a4f2b5de3ab814acc14b86e
Andres Lauder <camodres@gmail.com>
2022-12-07 19:09:48 -0800

    Background sound/size game fixes
    I made sure I could navigate throughout the entire game and get the different songs to change and work smoothly. I had to make sure some of the music stopped and others didn't. It gave me an awesome idea to produce some more background music depending on which game mode you decide to pick. I then worked on the size game mode and realized that when the ball is the smallest, it needs to go to the game over scene... I tried doing that by adding an if statement but noticed that the paddle was not getting to a value of 0, but something just a tad bit bigger (2.980232e-08f). So it still does not work but I need to test it....

--- Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Size-Paddle_Scripts/Size_Paddle_Script.cs
+++ Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Size-Paddle_Scripts/Size_Paddle_Script.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Size_Paddle_Script : MonoBehaviour
 {
     public Transform player_1;
     public Transform player_2;
     public Transform opponent;
+
+    private float smallest_paddle_size = 2.980232e-08f;
 
     void Start()
     {
@@ -66,18 +69,28 @@ public class Size_Paddle_Script : MonoBehaviour
         // scale of object is added with a new vector 3
         player_1.transform.localScale += new Vector3(0.2F, 0.2f, 0);
         yield return new WaitForSeconds(0);
+
+        if (player_1.transform.localScale == new Vector3(smallest_paddle_size, smallest_paddle_size, 1)){
+            SceneManager.LoadScene(2);
+        }
     }
 
     public IEnumerator Decrease_Paddle_Player2(){
         player_2.transform.localScale += new Vector3(0.2F, 0.2f, 0);
         yield return new WaitForSeconds(0);
-
+        if (player_2.transform.localScale == new Vector3(smallest_paddle_size, smallest_paddle_size, 1))
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     public IEnumerator Decrease_Paddle_Opponent()
     {
         opponent.transform.localScale += new Vector3(0.2F, 0.2f, 0);
         yield return new WaitForSeconds(0);
-
+        if (opponent.transform.localScale == new Vector3(smallest_paddle_size, smallest_paddle_size, 1))
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
