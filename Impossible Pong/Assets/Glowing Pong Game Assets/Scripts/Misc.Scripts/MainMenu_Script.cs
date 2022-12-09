d9b209be03c258fbf248a5efc008fbe23809acc5
Andres Lauder <camodres@gmail.com>
2022-12-09 00:17:49 -0800

    Player selection input/bug fixes
    I created a quick program that I attached to the MainMenu script that checks if the user clicks on the opp or p2 buttons. Depending on which they clicked, I have two images of pong paddles that become less transparent if one is selected, that way you can tell which button you selected. I also went in and made sure it was consistent between both scenes. One final thing I did was grow the exit button just so it is a little more visible for the player...

--- Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Misc.Scripts/MainMenu_Script.cs
+++ Impossible Pong/Assets/Glowing Pong Game Assets/Scripts/Misc.Scripts/MainMenu_Script.cs
@@ -26,7 +26,44 @@ public class MainMenu_Script : MonoBehaviour
     public ActivationScript impossible_p2_opp_act;
     public ActivationScript size_p2_opp_act;
 
+    // grabbing the two images
+    public Image opp_image;
+    public Image p2_image;
 
+    // grabbing bools of the images
+    private bool opp_image_bool = false;
+    private bool p2_image_bool = false;
+
+    // func() that sets a bool as true/false depending on what is clicked
+
+    public void Opp_and_p2_visibility_image()
+    {
+        // if opp is true, set opp image to half transparent
+        if (opp_image_bool == true)
+        {
+            // Set the opp color
+            var oppColor = opp_image.color;
+            oppColor.a = .60f;
+            opp_image.color = oppColor;
+            // set the p2 color
+            var p2Color = p2_image.color;
+            p2Color.a = 1f;
+            p2_image.color = p2Color;
+        }
+
+        // if p2 is true, set p2 image as half transparent
+        if (p2_image_bool == true)
+        {
+            // set the p2 color
+            var p2Color = p2_image.color;
+            p2Color.a = .60f;
+            p2_image.color = p2Color;
+            // set the opp color
+            var oppColor = opp_image.color;
+            oppColor.a = 1f;
+            opp_image.color = oppColor;
+        }
+    }
 
     // Regular Pong
     public void PongScene()
@@ -72,6 +109,9 @@ public class MainMenu_Script : MonoBehaviour
 
     public void Player2Activate()
     {
+        p2_image_bool = true;
+        opp_image_bool = false;
+
         reg_p2_opp_act.player2_active = true;
         reg_p2_opp_act.opponent_active = false;
 
@@ -91,6 +131,9 @@ public class MainMenu_Script : MonoBehaviour
 
     public void OpponentActivate()
     {
+        opp_image_bool = true;
+        p2_image_bool = false;
+
         reg_p2_opp_act.opponent_active = true;
         reg_p2_opp_act.player2_active = false;
 
